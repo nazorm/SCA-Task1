c242182a5db856d2e7a86e81388cac0c395ba868
Nazorm <nnadoziechinazor@gmail.com>
2020-06-28 12:10:51 +0100

    updated function to use Es6

--- col/col.js
+++ col/col.js
@@ -2,7 +2,7 @@ var colors = ["#FF5733", "#808000", "#00FF00", "#FA8072",  "#E9967A", "#999999"]
 var container = document.querySelector(".container")
 
 
-function changeColor(){
+const changeColor = () =>{
     var background = Math.floor(Math.random()*colors.length);
     container.style.backgroundColor = colors[background];
    
