f79d9ddf05a7d46a3084e84f88571e8245a419ea
Nazorm <nnadoziechinazor@gmail.com>
2020-06-28 11:30:34 +0100

    second branch

---
+++ qmain/man.js
@@ -0,0 +1,20 @@
+var quotes = ["Live and Love!", 
+
+"The purpose of our lives is to be happy. — Dalai Lama.",
+
+ "Life is what happens when you're busy making other plans. — John Lennon.",
+
+ "You only live once, but if you do it right, once is enough. — Mae West.",
+
+ "Keep calm and carry on. — Winston Churchill"
+
+
+    ];
+var quote = document.querySelector(".quote")
+
+
+function changeQuote(){
+    var newQuote = Math.floor(Math.random()*quotes.length);
+    quote.innerText = quotes[newQuote];
+   
+}
